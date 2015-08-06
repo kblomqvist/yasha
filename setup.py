f99d76b203f829c082174e3ea845e35b0ff95c76
Kim Blomqvist <kblomqvist@iki.fi>
2015-08-06 20:20:49 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="yasha",
+    version="0.1",
+    py_modules=["yasha"],
+    install_requires=[
+        "Click",
+        "Jinja2"
+    ],
+    entry_points='''
+        [console_scripts]
+        yasha=yasha:cli
+    ''',
+)

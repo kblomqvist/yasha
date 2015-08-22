7dda086489680768442c11ca0a4c496d84bbe7d5
Kim Blomqvist <kblomqvist@iki.fi>
2015-08-22 10:32:34 +0300

    Prepare for release

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="yasha",
+    author="Kim Blomqvist",
+    author_email="kblomqvist@iki.fi",
     version="1.0",
+    description="A command-line tool to render Jinja templates",
+    keywords=["jinja", "code generator"],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -15,4 +19,11 @@ setup(
         [console_scripts]
         yasha=yasha.scripts.yasha:cli
     ''',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    url="https://github.com/kblomqvist/yasha",
+    download_url="https://github.com/kblomqvist/yasha/tarball/1.0",
 )

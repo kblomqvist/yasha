66e8eb6f971904a4716f21d2e5d2148b48b34c0a
Kim Blomqvist <kblomqvist@iki.fi>
2017-09-10 18:18:09 +0300

    Add tests for template variables

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -170,45 +170,6 @@ def test_broken_extensions(tmpdir):
     assert b"Invalid syntax (foo.j2ext, line 1)" in e.value.output
 
 
-def test_inline_variable(tmpdir):
-    cwd = tmpdir.chdir()
-    tpl = tmpdir.join("template.j2")
-    tpl.write("{{ foo }}")
-
-    errno = call(["yasha", "--foo", "bar", "template.j2"])
-    assert errno == 0
-    assert path.isfile("template")
-
-    o = tmpdir.join("template")
-    assert o.read() == "bar"
-
-
-def test_explicit_inline_variable(tmpdir):
-    cwd = tmpdir.chdir()
-    tpl = tmpdir.join("template.j2")
-    tpl.write("{{ foo }}")
-
-    errno = call(["yasha", "--foo=bar", "template.j2"])
-    assert errno == 0
-    assert path.isfile("template")
-
-    o = tmpdir.join("template")
-    assert o.read() == "bar"
-
-
-def test_inline_variable_list(tmpdir):
-    cwd = tmpdir.chdir()
-    tpl = tmpdir.join("template.j2")
-    tpl.write("{{ foo }}")
-
-    errno = call(["yasha", "--foo=bar,baz", "template.j2"])
-    assert errno == 0
-    assert path.isfile("template")
-
-    o = tmpdir.join("template")
-    assert o.read() == "['bar', 'baz']"
-
-
 def test_stdin_and_out():
     cmd = ("echo -n \"foo\"", "|", "yasha", "-")
     out = check_output(cmd, shell=True)
---
+++ tests/test_template_variables.py
@@ -0,0 +1,68 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2015-2017 Kim Blomqvist
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import os
+import pytest
+from subprocess import call, check_output
+
+
+@pytest.fixture()
+def t(tmpdir):
+    return tmpdir.join('template.j2')
+
+
+def test_simple_variable(t):
+    t.write('{{ var is string }}, {{ var }}')
+    out = check_output(['yasha', '--var', 'foo', '-o-', str(t)])
+    assert out == b'True, foo'
+
+
+def test_list(t):
+    t.write('{{ var is sequence }}, {{ var | join }}')
+    out = check_output(['yasha', '--var', "['foo', 'bar', 'baz']", '-o-', str(t)])
+    assert out == b'True, foobarbaz'
+
+
+def test_tuple(t):
+    t.write('{{ var is sequence }}, {{ var | join }}')
+    out = check_output(['yasha', '--var', "('foo', 'bar', 'baz')", '-o-', str(t)])
+    assert out == b'True, foobarbaz'
+
+
+def test_dictionary(t):
+    t.write("{{ var is mapping }}, {% for k in 'abc' %}{{ var[k] }}{% endfor %}")
+    out = check_output(['yasha', '--var', "{'a': 1, 'b': 2, 'c': 3}", '-o-', str(t)])
+    assert out == b'True, 123'
+
+
+def test_number_type(t):
+    t.write('{{ var is number }}, {{ var + 1 }}')
+    out = check_output(['yasha', '--var', '1', '-o-', str(t)])
+    assert out == b'True, 2'
+
+
+def test_comma_separated_list(t):
+    t.write('{{ var is sequence }}, {{ var | join }}')
+    out = check_output(['yasha', '--var', 'foo,bar,baz', '-o-', str(t)])
+    assert out == b'True, foobarbaz'
